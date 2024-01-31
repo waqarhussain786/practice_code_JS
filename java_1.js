4df33462bb8e681852eaa4101fb4666042285274
waqarhussainujjan <wh751212@gmail.com>
2024-02-01 00:19:11 +0500

    notes

--- java_1.js
+++ java_1.js
@@ -1,9 +1,11 @@
-let a = "b";
-let b = "c";
-let c = "d";
+//let a = "b";
+//let b = "c";
+//let c = "d";
+//console.log(a);
+//document.getElementById("heading").innerHTML = "hello waqar";
 
 
-console.log(a);
-
-
-document.getElementById("heading").innerHTML = "hello waqar";
+let x, y, z;
+x = 5;
+y = 6;
+z = x + y;
