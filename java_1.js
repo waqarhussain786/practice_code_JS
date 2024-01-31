d3f05c1808ee76c8187959a14bb24b1bf490ceb5
waqarhussainujjan <wh751212@gmail.com>
2024-01-31 22:59:28 +0500

    element

--- java_1.js
+++ java_1.js
@@ -1,11 +1,1 @@
-console.log(['10'] * ['10']);
-
-import React from 'react'
-
-const java_1 = () => {
-  return (
-    <div>java_1</div>
-  )
-}
-
-export default java_1
+document.getElementById("heading").innerHTML = "hello waqar";
