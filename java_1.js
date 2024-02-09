81091458e09ac6156c0f199e37285073b16938e2
waqarhussainujjan <wh751212@gmail.com>
2024-02-09 18:44:13 +0500

    09-02-2024

--- java_1.js
+++ java_1.js
@@ -1,11 +1,3 @@
-//let a = "b";
-//let b = "c";
-//let c = "d";
-//console.log(a);
-//document.getElementById("heading").innerHTML = "hello waqar";
-
-
-let x, y, z;
-x = 5;
-y = 6;
-z = x + y;
+function changeHTML(){
+    document.getElementById('heading').innerHTML = "hello waqar hussain";
+}
