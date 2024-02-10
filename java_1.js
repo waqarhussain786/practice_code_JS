080381aedfbed12ba542d7c70c261feb811b7a10
waqarhussainujjan <wh751212@gmail.com>
2024-02-11 00:01:34 +0500

    11-02-2024

--- java_1.js
+++ java_1.js
@@ -1,17 +1,3 @@
-let a = 23;
-let b = 34;
-
-let firstName = "waqar";
-let lastName = "hussain";
-
-
-console.log("firstName" + " " + "lastName")
-
-
-console.log(a + b);
-
-
-
-function changeHTML() {
-    document.getElementById('heading').innerHTML = "hello waqar hussain";
-}
+let person = "waqar hussain ujjan";
+let carName = "Toyota";
+let price = 20;
