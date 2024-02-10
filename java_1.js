8f33a6ff1b9001658264d29561a06a54d131b1fc
waqarhussainujjan <wh751212@gmail.com>
2024-02-10 21:46:17 +0500

    10-02-2024

--- java_1.js
+++ java_1.js
@@ -1,3 +1,17 @@
-function changeHTML(){
+let a = 23;
+let b = 34;
+
+let firstName = "waqar";
+let lastName = "hussain";
+
+
+console.log("firstName" + " " + "lastName")
+
+
+console.log(a + b);
+
+
+
+function changeHTML() {
     document.getElementById('heading').innerHTML = "hello waqar hussain";
 }
