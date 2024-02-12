50c849f10c3d723add4f749571a3f10808c96ad1
waqarhussainujjan <wh751212@gmail.com>
2024-02-12 16:14:50 +0500

    12-02-2024

--- java_1.js
+++ java_1.js
@@ -1,7 +1,50 @@
-let button = document.getElementsByTagName("button")[0];;
+var input = document.getElementById("user_input");
+var button = document.getElementById("add");
+var ul = document.querySelector("ul")
 
-//document.getElementById("car").innerHTML = "cars";
 
 button.addEventListener("click", function () {
-    console.log("Clicked!!!");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var button = document.getElementById("enter");
+// var input = document.getElementById("userinput");
+// var ul = document.querySelector("ul");
+
+
+// button.addEventListener("click", function(){
+//     var li = document.createElement("li");
+//     li.appendChild(document.createTextNode(input.value));
+//     ul.appendChild(li);
+// })
+
+
+
+
+
+
+// button.addEventListener("click", function(){
+//     var li = document.createElement("li");
+//     li.appendChild(document.createTextNode("testing"));
+//     ul.appendChild(li);
+// })
+
