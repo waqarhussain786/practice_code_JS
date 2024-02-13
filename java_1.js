2d03d30db09e23fe8f059c2915665417987a0160
waqarhussainujjan <wh751212@gmail.com>
2024-02-13 19:03:57 +0500

    13-02-2024

--- java_1.js
+++ java_1.js
@@ -1,12 +1,31 @@
-var input = document.getElementById("user_input");
 var button = document.getElementById("add");
+var input = document.getElementById("user_input");
 var ul = document.querySelector("ul");
 
 
-button.addEventListener("click", function(){
+
+
+function AddItems() {
+    return input.value.length;
+}
+
+function createListElement() {
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
+    input.value = "";
+}
+
+button.addEventListener("click", function () {
+    if (AddItems() > 0) {
+        createListElement();
+    }
+});
+
+input.addEventListener("keypress", function (e) {
+    if (AddItems() > 0 && e.key === 'Enter') {
+        createListElement();
+    }
 });
 
 
@@ -23,16 +42,39 @@ button.addEventListener("click", function(){
 
 
 
-// var button = document.getElementById("enter");
-// var input = document.getElementById("userinput");
-// var ul = document.querySelector("ul");
 
 
-// button.addEventListener("click", function(){
-//     var li = document.createElement("li");
-//     li.appendChild(document.createTextNode(input.value));
-//     ul.appendChild(li);
-// })
+
+
+
+// button.addEventListener("click", function () {
+//     if(input.value.length > 0){
+//         var li = document.createElement("li");
+//         li.appendChild(document.createTextNode(input.value));
+//         ul.appendChild(li);
+//         input.value = "";
+//     }
+// });
+
+// input.addEventListener("keypress", function (e) {
+//     if(input.value.length > 0 && e.key === 'Enter'){
+//         var li = document.createElement("li");
+//         li.appendChild(document.createTextNode(input.value));
+//         ul.appendChild(li);
+//         input.value = "";
+//     }
+// });
+
+
+
+
+
+
+
+
+
+
+
 
 // button.addEventListener("click", function(){
 //     var li = document.createElement("li");
