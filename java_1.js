0f95ee43293e1dfe93acad4abb4a2ef02a4eaa52
waqarhussainujjan <wh751212@gmail.com>
2024-02-13 15:06:54 +0500

    13-02-2024

--- java_1.js
+++ java_1.js
@@ -3,17 +3,11 @@ var button = document.getElementById("add");
 var ul = document.querySelector("ul");
 
 
-
 button.addEventListener("click", function(){
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
-})
-
-
-
-
-
+});
 
 
 
