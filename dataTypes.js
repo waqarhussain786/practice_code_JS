33b1c43452aff7dc97352db194c1a9fb036d2a00
waqarhussainujjan <wh751212@gmail.com>
2024-03-30 22:22:50 +0500

    a

---
+++ dataTypes.js
@@ -0,0 +1,22 @@
+// primitive type
+// ===> 7 Types : string, number, boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+
+const scoreValue = 100.3;
+const isLoggedIN = false;
+const temp = null;
+
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+const bigNumber = 56464513216546321n
+
+
+
+// Reference type (Non-primitive type)
+// ===> Array, Objects, Functions
