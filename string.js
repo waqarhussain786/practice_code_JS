53730c2dd81baeb86c757c2443fea424cbcc50d1
waqarhussainujjan <wh751212@gmail.com>
2024-04-24 22:12:14 +0500

    a

--- string.js
+++ string.js
@@ -2,14 +2,15 @@ const Name = 'waqar hussain'
 const repoCount = 50;
 
 // console.log(Name + repoCount + "value");
-
 console.log(`hello my name is ${Name} and my repo count is ${repoCount}`)
 
 
-const newName = new String ('waqar hussain ujjan');
+const newName = new String('waqar hussain ujjan');
 
-console.log(newName.__proto__);
-console.log(newName.length);
-console.log(newName.toUpperCase());
-console.log(newName.charAt(3));
-console.log(newName.indexOf('r'));
+console.log(newName);
+
+// console.log(newName.__proto__);
+// console.log(newName.length);
+// console.log(newName.toUpperCase());
+// console.log(newName.charAt(3));
+// console.log(newName.indexOf('r'));
