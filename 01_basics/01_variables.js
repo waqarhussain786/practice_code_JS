ff54afb10381a822588d0c73b2ff480c3a100693
waqar hussain ujjan <wh751212@gmail.com>
2024-07-24 23:16:30 +0500

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 123;
+let accountEmail = "waqar@gmail.com";
+var accountPassword = "12345";
+accountCity = "Madeji";
+
+
+accountEmail = "ujjan@gmail.com"
+accountPassword = "1111"
+accountCity = "karachi"
+// accountId = 2; not allowed
+//console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
