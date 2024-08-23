0fae19975f390a6ac33ddce5ae85c5741176169d
waqar hussain ujjan <wh751212@gmail.com>
2024-08-23 15:01:02 +0500

    23-08-2024

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,15 +1,14 @@
-/*
+/* 
+    6 types of variables
+    1. numbers -----------------> 1234567890
+    2. string ------------------> "waqar hussain"
+    3. Boolean -----------------> 'true' OR 'false'
+    4. object ------------------> const x = {userName: 'xyz', userEmail: 'xyz@gmail.com', userPassword: '12345'}
+    5. arry object -------------> const x = ["xyz", "xyz@gmail.com", "12345"]
+    6. Date Object -------------> const date = new Date ("23/08/2024");
 
-Six types of variables
+ */
 
-1. Number ----------------> 1234567890
-2. String ----------------> "Hello"
-3. Boolean ---------------> True Or False
-4. Object ----------------> const userData = { firstName:"waqar", lastName: "hussain" }
-5. Arry Object -----------> const userData = ["waqar","hussain"]
-6. Date Object -----------> const date = new Date (2024/08/05)
-
-*/
 
 
 
